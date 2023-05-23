f0bcbc4c1b071919ba6b76b0e4b94db38b5b76e5
USER <USER@ZEPHYRUS>
2023-05-23 17:57:16 +0200

    Add project files.

---
+++ Kutyak2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello");
+var sorok = File.ReadAllLines("Datas\\Kutyak.csv");
+
+foreach (var sor in sorok)
+{
+    Console.WriteLine(sor);
+}
+Console.WriteLine();
